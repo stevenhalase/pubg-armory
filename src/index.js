da8d8b9b441216000403e682471ced831b0d5d5a
Steven Halase <steven.halase@gmail.com>
2018-04-05 02:56:30 -0500

    Added ga

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter } from 'react-router-dom';
 
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-80515700-2');
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
